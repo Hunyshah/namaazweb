d2d61a76dca275f113fe099e6247a0983def40bc
sarmad <sarmad.52422@gmail.com>
2023-08-31 00:09:14 +0500

    slider added

--- src/app/components/herofooter.tsx
+++ src/app/components/herofooter.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import EmblaCarousel from "embla-carousel/components/EmblaCarousel";
 
 type Props = {};
 
@@ -11,15 +12,15 @@ const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 5,
+    items: 1,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 1,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2,
+    items: 1,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -28,18 +29,10 @@ const responsive = {
 };
 
 const imageList = [
-  { id: "e1", image: "/images/slider1.jpeg" },
+  { id: "e1", image: "/images/slide.jpg" },
   {
     id: "e2",
-    image: "/images/slider2.jpeg",
-  },
-  {
-    id: "e3",
-    image: "/images/slider3.jpeg",
-  },
-  {
-    id: "e4",
-    image: "/images/slider4.jpeg",
+    image: "/images/slideimg.jpg",
   },
 ];
 
@@ -47,16 +40,43 @@ const Herofooter = (props: Props) => {
   return (
     <div className="main-parent flex justify-between bg-slate-950 text-white mt-10 ">
       <div className="flex flex-col items-center">
-        <div style={{marginLeft:50}}>
+        <div style={{ marginLeft: 50 }}>
           <div className="text-5xl font-bold "> Note </div>
           <div className=" mt-10">
-            <p className="text-4xl font-serif">Please turn off or put your cell phone on silent</p>
+            <p className="text-4xl font-serif">
+              Please turn off or put your cell phone on silent
+            </p>
           </div>
         </div>
       </div>
 
-      <div style={{ marginBottom: 10, marginRight: 50 }}>
-        <Image src={"/images/slide.jpg"} alt="ist" height={400} width={600} />
+      <div
+        style={{
+          marginBottom: 10,
+          marginRight: "5%",
+          width: "42%",
+          borderColor: "gray",
+          borderWidth: 1,
+          borderRadius:10,
+          padding: 10,
+        }}
+      >
+        <Carousel
+          responsive={responsive}
+          autoPlay={true}
+          autoPlaySpeed={5000}
+          rewind={true}
+          arrows={false}
+          transitionDuration={5000}
+        >
+          {imageList.map((item, index) => {
+            return (
+              <div key={index}>
+                <Image alt="slides" src={item.image} width={800} height={500} />
+              </div>
+            );
+          })}
+        </Carousel>
       </div>
 
       {/* <div className=' border-2 p-10 mr-1 border-white rounded-l-xl'>
