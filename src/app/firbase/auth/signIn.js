2f9eb02c560c134e084f68ed8d122888953da775
Your Name <you@example.com>
2023-09-22 22:10:24 +0500

    sign

--- src/app/firbase/auth/signIn.js
+++ src/app/firbase/auth/signIn.js
@@ -1,23 +1,32 @@
 import firebase_app from "../firebaseConfig";
-import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { signInWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
 
 const auth = getAuth(firebase_app);
 
-
 export default async function signIn(phone, password) {
-    let mEmail = `s${phone}@sunnybutt.com`
-    let result = null,
-        error = null;
-    try {
-        result = await signInWithEmailAndPassword(auth, mEmail, password);
-        console.log("Storing data in storage = "+phone);
-        localStorage.setItem("PHONE",phone);
-               
-     
-        
-    } catch (e) {
-        error = e;
-    }
+  let mEmail = `s${phone}@sunnybutt.com`
+  let result = null,
+      error = null;
+  try {
+    result = await signInWithEmailAndPassword(auth, mEmail, password);
+    console.log("Storing data in storage = "+phone);
+    localStorage.setItem("PHONE", phone);
+  } catch (e) {
+    error = e;
+  }
 
-    return { result, error };
+  return { result, error };
 }
+
+// Check if user is signed in on page load
+window.onload = function() {
+  onAuthStateChanged(auth, function(user) {
+    if (user) {
+      console.log("User is signed in");
+      // Do something if user is signed in
+    } else {
+      console.log("No user is signed in");
+      // Do something if no user is signed in
+    }
+  });
+};
